5d9bbcc92574e9a2f8cd5c2914ac888ed627c00c
unknown <pedrojok@pm.me>
2023-01-10 15:53:47 +0700

    Silence TS warning on build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    typescript: {
+        // NOT RECOMMANDED:
+        ignoreBuildErrors: true,
+    },
     eslint: {
         ignoreDuringBuilds: true,
     },
