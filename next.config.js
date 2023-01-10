0e114f6b01dda27caa9e2ee0de93974e247cb1a7
unknown <pedrojok@pm.me>
2023-01-10 17:34:28 +0700

    Remove withdrawSingleModal to fix casing

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    typescript: {
-        // NOT RECOMMENDED:
-        ignoreBuildErrors: true,
-    },
     eslint: {
         ignoreDuringBuilds: true,
     },
