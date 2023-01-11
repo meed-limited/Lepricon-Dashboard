18e2faec58caef6e853c1f43a09d1eaf9f27fd08
unknown <pedrojok@pm.me>
2023-01-11 15:59:31 +0700

    Rewrite api in next.congig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: "https://app.lepricon.io/:path*",
+            },
+        ];
+    },
     eslint: {
         ignoreDuringBuilds: true,
     },
