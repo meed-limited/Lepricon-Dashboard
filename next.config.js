a994239d2692de436be166d3bb8100a7eec0324f
unknown <pedrojok@pm.me>
2023-01-11 21:26:27 +0700

    Remove cors on internal API

--- next.config.js
+++ next.config.js
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,POST" },
-                    {
-                        key: "Access-Control-Allow-Headers",
-                        value: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
-                    },
-                    { key: "Content-Type", value: "application/json; charset=utf-8" },
-                ],
-            },
-        ];
-    },
     eslint: {
         ignoreDuringBuilds: true,
     },
