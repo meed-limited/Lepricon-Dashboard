c4525001c74d8a94f996b568f212c341a3ac9f19
unknown <pedrojok@pm.me>
2023-01-11 16:08:42 +0700

    Enable CORS in next.config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    async rewrites() {
+    async headers() {
         return [
             {
+                // matching all API routes
                 source: "/api/:path*",
-                destination: "https://app.lepricon.io/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,POST" },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
+                    },
+                    { key: "Content-Type", value: "application/json; charset=utf-8" },
+                ],
             },
         ];
     },
